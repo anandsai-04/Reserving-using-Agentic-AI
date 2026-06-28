01b99eb636c58355f07e866dc95a97059ef1165a
Anand Sai Gosiknda <anandsai2311@gmail.com>
2026-06-28 08:08:37 +0530

    Remove Clark and Mack models

--- backend/models/methods/__init__.py
+++ backend/models/methods/__init__.py
@@ -1,19 +1,15 @@
 from .chain_ladder import ChainLadder
-from .mack_chain_ladder import MackChainladder
 from .bornhuetter_ferguson import BornhuetterFerguson
 from .benktander import Benktander
 from .cape_cod import CapeCod
 from .case_outstanding import CaseOutstanding
-from .clark import Clark
 from .expected_loss_ratio import ExpectedLossRatio
 
 METHODS = {
     'CL': ChainLadder,
-    'MCL': MackChainladder,
     'BF': BornhuetterFerguson,
     'BK': Benktander,
     'CC': CapeCod,
     'CO': CaseOutstanding,
-    'CLK': Clark,
     'ELR': ExpectedLossRatio
 }
